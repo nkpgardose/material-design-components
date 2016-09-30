437cba086f315da2b09e7f8573e9ebb48b965429
Neil Kim Gardose <nkpgardose@gmail.com>
2016-09-30 16:05:36 +0800

    Add gulp "styles" task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,27 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 
 gulp.task('default', () => {
   console.log('Performing gulp tasks...');
 });
+
+gulp.task('styles', () => {
+  console.log('Doing some styles tasks...');
+  return gulp.src('stylesheets/*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
