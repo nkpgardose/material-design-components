193643d85cca7423674f0b3ca3edf6af2d65a45b
Neil Gardose <nkpgardose@gmail.com>
2016-10-01 20:05:45 +0800

    Add set of gulp task for production and for tmp serve
    - Rename `styles` to `stylesheets`.
    - Add new gulp task `serve` to serve for development. Using live reload
    - Add new gulp for tmp, it is being use for `gulp serve`

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,60 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
+import del from 'del';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-gulp.task('default', () => {
-  console.log('Performing gulp tasks...');
+gulp.task('default', ['clean'], () => {
+  gulp.start('dist');
 });
 
-gulp.task('styles', () => {
-  console.log('Doing some styles tasks...');
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('dist', ['html'], () => {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('html', ['stylesheets'], () => {
+  return gulp.src('*.html')
+    .pipe($.useref({searchPath: ['.tmp', '.']}))
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.cssnano()))
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'stylesheets'}));
+});
+
+gulp.task('stylesheets', () => {
+  return gulp.src('stylesheets/*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      precision: 10,
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.cssnano())
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist/stylesheets'));
+});
+
+gulp.task('serve', ['stylesheets:tmp'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', '.'],
+    }
+  });
+
+  gulp.watch([
+    '*.html',
+  ]).on('change', reload);
+
+  gulp.watch('stylesheets/**/*.scss', ['stylesheets:tmp']);
+});
+
+gulp.task('stylesheets:tmp', () => {
   return gulp.src('stylesheets/*.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
@@ -21,7 +65,7 @@ gulp.task('styles', () => {
     }).on('error', $.sass.logError))
     .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(gulp.dest('.tmp/stylesheets'))
     .pipe(reload({stream: true}));
 });
 
